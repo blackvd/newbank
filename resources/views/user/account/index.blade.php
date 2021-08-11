aadd34b8beb4316fb8eb5ad4501de5bc15991e3e
blackvd <aristideehouman@yahoo.fr>
2021-08-11 05:13:00 +0000

    virement & Demandes --front end

--- resources/views/user/account/index.blade.php
+++ resources/views/user/account/index.blade.php
@@ -4,6 +4,10 @@
 
 @section('styles')
 <link href="{{asset('assets/css/components/tabs-accordian/custom-tabs.css')}}" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="{{asset('plugins/bootstrap-select/bootstrap-select.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/elements/alert.css')}}">
+<link href="{{asset('plugins/flatpickr/flatpickr.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('plugins/flatpickr/custom-flatpickr.css')}}" rel="stylesheet" type="text/css">
 @endsection
 
 @section('css-apply-to-body', 'alt-menu sidebar-noneoverflow')
@@ -41,15 +45,13 @@
                         <div class="dropdown-menu">
                             <a class="dropdown-item" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Compte à compte</a>
                             <a class="dropdown-item"  id="profile-tab2" data-toggle="tab" href="#profile2" role="tab" aria-controls="profile2" aria-selected="false">Entre comptes NewBank</a>
-                            <a class="dropdown-item"  id="profile-tab3" data-toggle="tab" href="#profile3" role="tab" aria-controls="profile3" aria-selected="false">Vers autres banques</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Demandes <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">RIB</a>
-                            <a class="dropdown-item"  id="profile-tab2" data-toggle="tab" href="#profile2" role="tab" aria-controls="profile2" aria-selected="false">Relevé de compte</a>
-                            <a class="dropdown-item"  id="profile-tab3" data-toggle="tab" href="#profile3" role="tab" aria-controls="profile3" aria-selected="false">Vers autres banques</a>
+                            <a class="dropdown-item" id="rib-tab" data-toggle="tab" href="#rib" role="tab" aria-controls="rib" aria-selected="false">RIB</a>
+                            <a class="dropdown-item"  id="bilan-tab" data-toggle="tab" href="#bilan" role="tab" aria-controls="bilan" aria-selected="false">Relevé de compte</a>
                         </div>
                     </li>
                     {{-- <li class="nav-item">
@@ -84,19 +86,103 @@
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                <div class="media">
-                                    <img class="mr-3" src="assets/img/profile-32.jpg" alt="Generic placeholder image">
-                                    <div class="media-body">
-                                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                                @if(count($account_s) <= 1)
+                                    <div class="alert alert-light-danger border-0 mb-4" role="alert">
+                                        <strong>Fonctionnalité indisponible ! </strong> vous n'avez qu'un seul compte
                                     </div>
-                                </div>
+                                @else
+                                    <form action="">
+                                        <div class="form-group row mb-4">
+                                            <label for="account_debit" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Compte à débiter</label>
+                                            <div class="col-xl-10 col-lg-9 col-sm-10">
+                                                <select name="account_debit" id="account_debit" class="form-control selectpicker">
+                                                    @foreach($account_s as $account)
+                                                        <option value="{{$account->id}}">{{$account->numero_compte}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row mb-4">
+                                            <label for="account_credit" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Compte à créditer</label>
+                                            <div class="col-xl-10 col-lg-9 col-sm-10">
+                                                <select name="account_credit" id="account_credit" class="form-control selectpicker">
+
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row mb-4">
+                                            <label for="amount" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Montant(FCFA)</label>
+                                            <div class="col-xl-10 col-lg-9 col-sm-10">
+                                                <input type="number" class="form-control" id="amount" placeholder="12000">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-10">
+                                                <button type="submit" class="btn btn-primary mt-3">Valider</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                @endif
                             </div>
                             <div class="tab-pane fade" id="profile2" role="tabpanel" aria-labelledby="profile-tab2">
-                                <p class="">
-                                    Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                </p>
+                                <form action="">
+                                    <div class="form-group row mb-4">
+                                        <label for="account_debit" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Compte à débiter</label>
+                                        <div class="col-xl-10 col-lg-9 col-sm-10">
+                                            <select name="account_debit" id="account_debit" class="form-control selectpicker">
+                                                @foreach($account_s as $account)
+                                                    <option value="{{$account->id}}">{{$account->numero_compte}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label for="account_credit" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Compte à créditer</label>
+                                        <div class="col-xl-10 col-lg-9 col-sm-10">
+                                            <input type="text" class="form-control" name="account_credit" id="account_credit">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label for="amount" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Montant(FCFA)</label>
+                                        <div class="col-xl-10 col-lg-9 col-sm-10">
+                                            <input type="number" class="form-control" id="amount" placeholder="12000">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-sm-10">
+                                            <button type="submit" class="btn btn-primary mt-3">Valider</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="tab-pane fade" id="rib" role="tabpanel" aria-labelledby="rib-tab">
+                                <form action="">
+                                    <div class="form-group row mb-4">
+                                        <label for="hEmail" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Options</label>
+                                        <div class="col-xl-10 col-lg-9 col-sm-10">
+                                            <select class="selectpicker form-control">
+                                                <option>Télécharger</option>
+                                                <option>Recevoir par mail</option>
+                                                <option>Télécharger & recevoir par mail</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary mt-3">Soumettre</button>
+                                </form>
+                            </div>
+                            <div class="tab-pane fade" id="bilan" role="tabpanel" aria-labelledby="bilan-tab">
+                                <form action="" class="form-inline justify-content-center">
+                                    <label for="period" class="mr-2">Période</label>
+                                    <input id="period" class="form-control flatpickr flatpickr-input active mr-2" type="text" placeholder="Select Date..">
+                                    <button type="submit" class="btn btn-primary">Valider</button>
+                                </form>
                             </div>
                         </div>
             </div>
@@ -105,8 +191,15 @@
 </div>
 @endsection
 
-
-
-
 @section('scripts')
+    <script src="{{asset('plugins/bootstrap-select/bootstrap-select.min.js')}}"></script>
+    <script src="{{asset('plugins/input-mask/jquery.inputmask.bundle.min.js')}}"></script>
+    <script src="{{asset('plugins/input-mask/input-mask.js')}}"></script>
+    <script src="{{asset('plugins/flatpickr/flatpickr.js')}}"></script>
+    <script>
+        $("#account_credit").inputmask({mask:"CI221019999999999"});
+        var period = flatpickr($("#period"), {
+            mode: "range"
+        })
+    </script>
 @endsection
