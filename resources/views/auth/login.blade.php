4ed42070c2b0f90b61318b1093adca844844c15f
link <bazaroov@gmail.com>
2021-11-10 04:15:29 +0000

    correction de btn unitile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,15 +61,6 @@
                                     <input id="name" name="name" type="text" class="form-control" placeholder="Nom d'utilisateur">
                                 </div>
 
-                                <div id="password-field" class="field-wrapper input mb-2">
-                                    <div class="d-flex justify-content-between">
-                                        <label for="password">MOT DE PASSE</label>
-                                        <a href="auth_pass_recovery_boxed.html" class="forgot-pass-link">Mot de passe oublié?</a>
-                                    </div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
-                                    <input id="password" name="password" type="password" class="form-control" placeholder="Mot de passe">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                                </div>
                                 <div class="d-sm-flex justify-content-between">
                                     <div class="field-wrapper">
                                         <button type="submit" class="btn btn-primary" value="">Connexion</button>
